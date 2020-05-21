807b25c20b24201aa9553d995da6f528073d6c95
Georgi Vutev <61160680+SpectraPHBG@users.noreply.github.com>
2020-05-21 18:42:02 +0300

    commit

--- OOP Intermediate HW/Task2/Program.cs
+++ OOP Intermediate HW/Task2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
@@ -9,39 +9,39 @@ namespace Task2
     {
         class Car
         {
-            public int HorsePower { get; set; }
+            public double HorsePower { get; set; }
             public string Brand { get; set; }
             public int Weight { get; set; }
-            public decimal AccelerationTo100 { get; set; }
-            public decimal AccelerationFrom100To200 { get; private set; }
+            public double AccelerationTo100 { get; set; }
+            public double AccelerationFrom100To200 { get; private set; }
             public int Boost
             {
                 set
                 {
                     if (value == 100)
                     {
-                        AccelerationTo100 = (decimal)1000 / (2 * HorsePower);
-                        AccelerationTo100 = AccelerationTo100 - (AccelerationTo100 * 30) / 100;
-                        AccelerationFrom100To200 = (decimal)1000 / HorsePower;
+                        AccelerationTo100 = 1000 / (2 * HorsePower);
+                        AccelerationTo100 = AccelerationTo100 - ((AccelerationTo100 * 30) / 100);
+                        AccelerationFrom100To200 = 1000 / HorsePower;
 
                     }
                     else if (value == 200)
                     {
-                        AccelerationTo100 = (decimal)1000 / (2 * HorsePower);
-                        AccelerationFrom100To200 = (decimal)1000 / HorsePower;
-                        AccelerationFrom100To200 = AccelerationFrom100To200 - (AccelerationFrom100To200 * 20) / 100;
+                        AccelerationTo100 = 1000 / (2 * HorsePower);
+                        AccelerationFrom100To200 = 1000 / HorsePower;
+                        AccelerationFrom100To200 = AccelerationFrom100To200 - ((AccelerationFrom100To200 * 20) / 100);
                     }
                     else
                     {
-                        AccelerationTo100 = (decimal)1000 / (2 * HorsePower);
-                        AccelerationFrom100To200 = (decimal)1000 / HorsePower;
+                        AccelerationTo100 = 1000 / (2 * HorsePower);
+                        AccelerationFrom100To200 = 1000 / HorsePower;
                     }
                     AccelerationTo100 = Math.Round(AccelerationTo100, 2);
                     AccelerationFrom100To200 = Math.Round(AccelerationFrom100To200, 2);
                 }
             }
 
-            public Car(string brand, int horsePower, int weight, int boost)
+            public Car(string brand, double horsePower, int weight, int boost)
             {
                 this.Brand = brand;
                 this.HorsePower = horsePower;
@@ -55,7 +55,7 @@ namespace Task2
             public string LastName { get; set; }
             public Car MyCar { get; set; }
             public int Points { get; set; }
-            public Participant(string firstName, string lastName, string carBrand, int weight, int horsePower, int boost)
+            public Participant(string firstName, string lastName, string carBrand, int weight, double horsePower, int boost)
             {
                 this.FirstName = firstName;
                 this.LastName = lastName;
@@ -77,7 +77,7 @@ namespace Task2
                 string lastName = inputData[1];
                 string brand = inputData[2];
                 int weight = int.Parse(inputData[3]);
-                int horsePower = int.Parse(inputData[4]);
+                double horsePower = double.Parse(inputData[4]);
                 int boost = 0;
                 if (inputData[5].ToLower() == "do-100") boost = 100;
                 if (inputData[5].ToLower() == "do-200") boost = 200;
